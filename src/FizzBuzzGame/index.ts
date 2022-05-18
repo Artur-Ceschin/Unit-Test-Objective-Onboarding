9beacba4c014e7e651af81e3a5c5a74f5f610ee0
Artur Ceschin <artur.ceschin@gmail.com>
2022-05-18 12:13:36 -0300

    Second class completed

--- src/FizzBuzzGame/index.ts
+++ src/FizzBuzzGame/index.ts
@@ -1,7 +1,19 @@
 export default class FizzBuzzGame {
   play(value: number): string {
+    if (value % 2 === 0 && value % 3 === 0 && value % 5 === 0) {
+      return 'FizzBuzzFoo'
+    }
+    if (value % 2 === 0 && value % 5 === 0) {
+      return 'BuzzFoo'
+    }
+    if (value % 2 === 0 && value % 3 === 0) {
+      return 'BuzzFoo'
+    }
     if (value % 3 === 0 && value % 5 == 0) {
       return 'FizzBuzz'
+    }
+    if (value % 2 === 0) {
+      return 'FizzFoo'
     }
     if (value % 3 === 0) {
       return 'Fizz'
